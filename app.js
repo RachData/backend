12c2de397144b32c992ba63db2e480d1edf0bd51
RachData <rachidderra@gmail.com>
2020-03-18 02:12:42 +0100

    creation des utilisateur et du hash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const stuffRoutes= require('./routes/stuff');
+const userRoutes = require('./routes/Users');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/api/stuff',stuffRoutes)
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
